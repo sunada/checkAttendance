8bd7191c51e7304ef20eb77d6c40fcb4334d0d3e
sunada <sunada2005@163.com>
2013-07-02 20:16:38 +0800

    use xlrd

--- checkAttendance.py
+++ checkAttendance.py
@@ -1,15 +1,42 @@
 import xlrd
+from openpyxl import load_workbook
+import time
 
-def PickEmployee(filename):
+#openpyxl will spend too much time
+def testOpenpyxl(filename):
+    start=time.time()
+    wb2=load_workbook(filename)
+    print wb2.get_sheet_names() 
+    print 'openpyxl: ',time.time()-start
+    
+
+#xlrd is much more efficent
+def testXlrd(filename):
+    start=time.time()
     book=xlrd.open_workbook(filename)
-    print "The number of worksheets is ",book.nsheets
     print "Worksheet name(s): ",book.sheet_names()
+    print 'xlrd: ', time.time()-start
+    '''
     sh=book.sheet_by_index(0)
     print sh.name, sh.nrows, sh.ncols
     print "Cell D30 is", sh.cell_value(rowx=29, colx=3)
     for rx in range(sh.nrows):
         print sh.row(rx)
-    
+    '''
 
+def pickMember(filename):
+    book=xlrd.open_workbook(filename)
+    sh=book.sheet_by_index(0)
+    print 'rows: ', sh.nrows
+    print 'columns: ', sh.ncols
+    #print 'A15: ', sh.cell_value(rowx=14,colx=0).encode('gbk')
+    
+    for rx in range(sh.nrows):
+        print 'A'+str(rx)+': ', sh.cell_value(rowx=rx,colx=0).encode('gbk')
+    
+    
 if __name__=='__main__':
-    PickEmployee('June.xls')
+    #testXlrd('June.xls')
+    #testOpenpyxl('June.xlsx')
+    pickMember('June.xls')
+

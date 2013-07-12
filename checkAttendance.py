932a9b0941ce24af4c25d75f6b859a5752471aab
unknown <sunada2005@163.com>
2013-07-12 15:27:54 +0800

    checkAttendance.py

--- checkAttendance.py
+++ checkAttendance.py
@@ -13,6 +13,8 @@ depColx=0
 deps=[u'安全软件测试与支持部',u'安管标准化产品及平台部',
 u'安管行业化产品部',u'安管互联网产品部',u'安全软件技术支持部',
 u'大数据分析与安全研究部']
+#import time point: 8:00,10:00,10:30,16:00,16:30,19:00,20:00
+tpnts=[0.333333,0.416667,0.4375,0.666666,0.6875,0.791667,0.833333]
 
 '''
 #openpyxl will spend too much time
@@ -78,31 +80,59 @@ def pickMember(fileRead,fileWrite):
         #print 'A'+str(rx)+': ', sh.cell_value(rowx=rx,colx=depColx).encode('gbk')
         dep=shr.cell_value(rowx=rx,colx=depColx)
         if dep in deps:
-            for cx in range(shr.ncols-2):
+            for cx in range(shr.ncols-3):
                 shw.write(cnt,cx,shr.cell_value(rx,cx))
-            hours=shr.cell_value(rx,cx)*24
-            first=shr.cell_value(rx,cx-2)
-            last=shr.cell_value(rx,cx-1)
-            print type(hours)
+            first=shr.cell_value(rx,cx-1)
+            last=shr.cell_value(rx,cx)
+            #print type(hours)
             if type(first) is unicode:
-                shw.write(cnt,cx+2,u'迟到')
-                shw.write(cnt,cx+3,u'早退')
-                shw.write(cnt,cx+4,u'工时不足')
-                shw.write(cnt,cx+5,u'旷工')
+                shw.write(cnt,cx+1,u'上班考勤')
+                shw.write(cnt,cx+2,u'下班考勤')
+                shw.write(cnt,cx+3,u'考勤工时')
+                shw.write(cnt,cx+4,u'是否工时不足')
+                #shw.write(cnt,cx+5,u'旷工') 
             elif type(first) is float:
-                shw.write(cnt,cx+1,hours)
-                print first-0.416667
-                print first-0.4375
-                print last-0.6875
-                print hours
-                if first-0.416667>0:
-                    shw.write(cnt,cx+2,'Yes')
-                if first-0.4375<0:
-                    shw.write(cnt,cx+5,'Yes')
-                if last-0.6875<0:
-                    shw.write(cnt,cx+3,u'Yes')
+                if first<tpnts[0]:
+                    hours=(last-tpnts[0])*24
+                else:
+                    hours=(last-first)*24
+                shw.write(cnt,cx+3,hours)
+                #10:31-15:59叫旷工
+                if first-tpnts[2]>0:
+                    print 'KG'
+                    print 'first:',first*24
+                    print '10:30:',tpnts[2]
+                    shw.write(cnt,cx+1,u'***旷工***')
+                #10:01至10:30之间叫迟到
+                elif first-tpnts[1]>0:
+                    print 'CD'
+                    print 'first:',first*24,' 10:00',tpnts[1]
+                    shw.write(cnt,cx+1,u'***迟到***')
+                else:
+                    print 'first ZH',first*24
+                    shw.write(cnt,cx+1,u'正常')
+                
+                #10:31-15:59叫旷工
+                if last-tpnts[3]<0:
+                    print 'KG'
+                    print 'last:',last*24
+                    print '16:00:',tpnts[3]
+                    shw.write(cnt,cx+2,u'***旷工***')
+                #16：00至16：29之间叫早退
+                elif last-tpnts[4]<0:
+                    print 'ZT'
+                    print 'last:',last,' 16:30,',tpnts[4]
+                    shw.write(cnt,cx+2,u'***早退***')
+                else:
+                    print 'last ZH',last*24
+                    shw.write(cnt,cx+2,u'正常')
+                #小时8.5小时叫工时不足
                 if hours<8.5:
-                    shw.write(cnt,cx+4,u'Yes')
+                    print 'hours: ',hours
+                    shw.write(cnt,cx+4,u'工时不足')
+                else:
+                    print 'hours: ',hours
+                    shw.write(cnt,cx+4,u'工时足够')
             else:
                 pass
             
@@ -115,5 +145,5 @@ if __name__=='__main__':
     #testXlrd('June.xls')
     #testOpenpyxl('June.xlsx')
     #testXlwt('xlwt.xls')
-    pickMember('June.xls','treated.xls')
+    pickMember('June.xls','treated1.xls')
 
